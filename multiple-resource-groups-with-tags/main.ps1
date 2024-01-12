9a98663df78b478d7fd9d93e45b411d512ae0cb1
Godswill Ekpe <132562475+geenicetech@users.noreply.github.com>
2024-01-12 10:53:52 +0100

    edited script

--- multiple-resource-groups-with-tags/main.ps1
+++ multiple-resource-groups-with-tags/main.ps1
@@ -1,14 +1,14 @@
 $location = 'east us'
 $resourcegroup = 'RG1','RG2','RG3'
 
-foreach ($rgname in $resourcegroup)
+foreach ($rg in $resourcegroup)
 {
-New-AzResourceGroup -name $rgname -location $location -Tag @{department = 'sales' ; office = 'abuja'}
+New-AzResourceGroup -name $rg -location $location -Tag @{department = 'sales' ; office = 'abuja'}
 }
 
 
 #Delete resource groups
-foreach ($rgname in $resourcegroup)
-{Remove-AzResourceGroup -Name $rgname -Force}
+foreach ($rg in $resourcegroup)
+{Remove-AzResourceGroup -Name $rg -Force}
 
 
